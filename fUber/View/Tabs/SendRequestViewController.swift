e155884d07495359577f75c117d12cc7e40436bd
Andriy Dmytruk <Andriyd909@gmail.com>
2019-01-27 18:00:26 +0000

    Added normal date picker

--- fUber/View/Tabs/SendRequestViewController.swift
+++ fUber/View/Tabs/SendRequestViewController.swift
@@ -17,40 +17,75 @@ class SendRequestViewController: UIViewController {
     @IBOutlet weak var timeField: UIResizedTextField!
     @IBOutlet weak var validTimeField: UIResizedTextField!
     
+    weak var currentTimeField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var doneButton: UIRoundedColoredButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        timeField?.delegate = self
+        validTimeField?.delegate = self
 
         // Do any additional setup after loading the view.
     }
     
     // MARK: Actions
+    
+    func moveDateToTextField() {
+        guard let timeField = self.currentTimeField else {
+            return
+        }
+        
+        let date = datePicker.date
+        let dateString = Request.formatDate(date)
+        timeField.text! = dateString
+    }
+    
+    @IBAction func dateChanged(_ sender: Any) {
+        moveDateToTextField()
+    }
+    
+    @IBAction func done(_ sender: Any) {
+        doneButton.removeFromSuperview()
+        datePicker.removeFromSuperview()
+    }
+    
     @IBAction func sendRequest(_ sender: Any) {
         guard let locationFrom = addressFromField.text,
             let locationTo = addressToField.text,
-            let time = timeField.text,
-            let validTime = validTimeField.text else {
+            let timeString = timeField.text,
+            let validTimeString = validTimeField.text else {
                 return
         }
         
-        do {
-            guard let time = try Request.getDate(string: time),
-                let validTime = try Request.getDate(string: validTime) else {
-                    return
-            }
+        guard let time = Request.getDate(string: timeString),
+            let validTime =  Request.getDate(string: validTimeString) else {
+                return
+        }
             
-            RequestsService.instance.addRequest(Request(userid: nil, time: time, validTime: validTime, locationFrom: locationFrom, locationTo: locationTo))
-        } catch {}
-    }
-    
-    /*
-    // MARK: - Navigation
+    RequestsService.instance.addRequest(Request(userid: nil, time: time, validTime: validTime, locationFrom: locationFrom, locationTo: locationTo))
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
+}
 
+extension SendRequestViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        self.currentTimeField = textField
+        
+        self.view.addSubview(doneButton)
+        self.view.addSubview(datePicker)
+        
+        self.view.addConstraints([
+            NSLayoutConstraint(item: doneButton, attribute:  .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottomMargin, multiplier: 1, constant: -4),
+            NSLayoutConstraint(item: doneButton, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: datePicker, attribute: .left, relatedBy: .equal, toItem: self.view, attribute: .left, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: datePicker, attribute: .right, relatedBy: .equal, toItem: self.view, attribute: .right, multiplier: 1, constant: 0),
+            NSLayoutConstraint(item: datePicker, attribute: .bottom, relatedBy: .equal, toItem: doneButton, attribute: .top, multiplier: 1, constant: -4),
+            ])
+        
+        moveDateToTextField()
+        
+        return false
+    }
 }
