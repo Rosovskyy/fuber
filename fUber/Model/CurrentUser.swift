97ff39badbe4e0ed7857be97bd571b63650363d7
Serhiy Rosovskyy <serhiyrosovskyy@Serhiys-MacBook-Air.local>
2019-01-28 21:57:06 +0200

    User's data

--- fUber/Model/CurrentUser.swift
+++ fUber/Model/CurrentUser.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+
+class CurrentUser {
+    let uid: String
+    let name: String
+    let number: String
+    let email: String
+    
+    init(uid: String, dictionary: [String: Any]) {
+        self.uid = uid
+        self.name = dictionary["name"] as? String ?? ""
+        self.number = dictionary["number"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+    }
+}
